a977275e271ae50fa53d7ed2c5bf4ec18d62f7ee
shifuthr <132672086+shifuthr@users.noreply.github.com>
2025-04-09 13:30:20 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+document.getElementById('closePopup').addEventListener('click', function () {
+    document.getElementById('popupAd').style.display = 'none';
+});
+
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById('popupAd').style.display = 'flex';
+    }, 2000);
+};
+Write to GROUP 3 w/o CI 2nd Sem
